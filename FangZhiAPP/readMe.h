8e400f4b794195a118d0f613679054f23e0c61fb
dorayo <yomingyo@gmail.com>
2020-03-12 09:08:39 +0800

    lll

---
+++ FangZhiAPP/readMe.h
@@ -0,0 +1,23 @@
+//
+//  readMe.h
+//  FangZhiAPP
+//
+//  Created by zk on 2019/12/19.
+//  Copyright © 2019 张坤. All rights reserved.
+//
+
+#ifndef readMe_h
+#define readMe_h
+
+
+
+/**
+苹果账号: webmaster@yarnscolors.com
+   密码: YARNS&colors.com5116
+ 
+ 
+ */
+
+
+#endif /* readMe_h */
+
